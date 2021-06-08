84b65454c2858b08757beab97ac6a116d813a966
Hunterpp <hunter.p@h.p>
2021-06-07 20:44:38 -0400

    distilled code

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var createRoom = require('../lib/createroom');
-var joinRoom = require('../lib/joinroom');
-var listMedia = require('../lib/mediaList');
-var kaltura = require('kaltura-client');
+
+const kaltura = require('kaltura-client');
+const scheduleEvent = require('../lib/scheduleEvent');
+const scheduleEventResource = require('../lib/scheduleEventResource');
+const createRoom = require('../lib/createRoom');
+const joinRoom = require('../lib/joinRoom');
 const KalturaClientFactory = require('../lib/kalturaClientFactory');
+const addMedia = require('../lib/mediaEntry');
 
+const { v4: uuidv4 } = require('uuid');
 
-/* GET home page. */
 router.get('/', async function (req, res, next) {
-  let adminKs = await KalturaClientFactory.getKS(process.env.KALTURA_USER_ID);
-  let media = await listMedia(adminKs, req.cookies.sched_uuid);
-  console.log("MK:" + media.totalCount);
+  let adminKs = await KalturaClientFactory.getKS(process.env.KALTURA_USER_ID,
+    { type: kaltura.enums.SessionType.ADMIN });
 
-  res.render('index', { sched: "asd" });
-});
+  //create room aka resource
+  let room = await createRoom(adminKs, "Room Topic");
 
-/* POST */
-router.post('/', async function (req, res, next) {
-  let adminKs = await KalturaClientFactory.getKS(process.env.KALTURA_USER_ID);
+  //create mediaentry as template
+  let media = await addMedia(adminKs, "The tag");
 
-  let adhocUUID = uuidv4();
-  res.cookie('adhoc_uuid', adhocUUID);
+  //create schedule event
+  let now = Math.floor(Date.now() / 1000);
+  let end = now + 10800;
+  let event = await scheduleEvent(adminKs, "Event Topic", now, end, media.id);
 
-  createRoom(adminKs, req.body.question + " " + adhocUUID).then(room => {
-    console.log("creating room with: " + room);
-    let adminRoom = joinRoom(null, room.id,
-      true,
-      "Adminx Name",
-      "Adminx Last Name",
-      "admi3n@admin.admin");
+  //associate room with event
+  await scheduleEventResource(adminKs, event.id, room.id);
 
-    let studentRoom = joinRoom(null, room.id,
-      false,
-      req.body.firstName,
-      req.body.lastName,
-      req.body.email);
+  let adminRoom = await joinRoom(event.id, null,
+    true,
+    "Admin Name",
+    "Admin Last Name",
+    "admin@admin.admin");
 
-    Promise.all([adminRoom, studentRoom]).then((values) => {
-      console.log("ALL");
-      console.log(values);
-      console.log("ALLD");
-      res.render('start', {
-        adminUrl: values[0],
-        studentUrl: values[1]
-      });
-    });
+  res.render('index', {
+    adminUrl:adminRoom
   });
+
 });
 
 module.exports = router;
